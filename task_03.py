67f8af1c62902eb8145f5829b9e1693e780fa339
pcwebb <pamela.webb@spsmail.cuny.edu>
2016-04-11 22:32:29 +0000

    Week 12 Warmup

--- task_03.py
+++ task_03.py
@@ -7,23 +7,37 @@ import time
 
 
 class CustomLogger(object):
+    """A custom log class"""
 
     def __init__(self, logfilename):
+        """A constructor for CustomLogger class"""
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """A logging function"""
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """A flush function"""
         handled = []
 
-        fhandler = open(self.logfilename, 'a')
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError as err:
+            self.log('Cannot be opened', time.time)
+            raise err
         for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
+            try:
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+            except IOError:
+                self.log('IO error', time.time)
+                break
+            except BaseException:
+                self.log('Any other error', time.time)
 
         fhandler.close()
 
