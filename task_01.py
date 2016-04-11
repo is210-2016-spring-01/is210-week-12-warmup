38ec17785f351e6fd44de797f7b8d499dd62b210
William Cole <billy.cole@gmail.com>
2016-04-11 09:44:03 -0400

    Simple Lookup

--- task_01.py
+++ task_01.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Task 01 module"""
+"""simple_lookup"""
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """Function that looks up keys and lists
+    Attributes:
+        var1 (list): A list of items
+        var2 (int): Index of list.
+    Returns:
+        var1[var2]: The element and key are returned as var1[var2]
+    Example:
+        >>> simple_lookup([1,3], 1)
+        3
+    """
+
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: Your index/key doesn\'t exist.'
+        return var1
