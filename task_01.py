70b142294562e19c5cb9c936749aaf452fb3f4ad
Bill Sass <william.sass@cpsmail.cuny.edu>
2016-04-11 23:38:45 +0000

    Completed week 12 warmup tasks

--- task_01.py
+++ task_01.py
@@ -4,4 +4,27 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """ Sample function for testing exceptino handling.
+
+    Args:
+        var1 (string, list: or dictinary): object to be indexed or searched.
+        var2 (int or key): index value or key to use for var1.
+
+    Returns:
+        (mixed): either result from index operation, value of key, or if
+                an exception is raised, var1
+    Examples:
+        >>> s = 'this is a sample string'
+        >>> d = {'a': 'simple', 'd': 'dictionary'}
+        >>> simple_lookup(d, 'a')
+        simple
+
+        >>> simple_lookup(s, 20)
+        Warning: index/key 20 doesn't exist
+        'this is a sample string'
+    """
+    try:
+        return var1[var2]
+    except LookupError:
+        print 'Warning: index/key {0} doesn\'t exist'.format(var2)
+        return var1
--- task_03.py
+++ task_03.py
@@ -7,25 +7,81 @@ import time
 
 
 class CustomLogger(object):
+    """ Custom Logger class for testing the finally process.
+
+    Args:
+        none.
+
+    Attributes:
+        none.
+    """
 
     def __init__(self, logfilename):
+        """ constructor for CustomerLogger testing class
+
+        Args:
+            logfilename (string): name of file to receive logging messages
+
+        Attributes:
+            logfilename (string): name of file to receive logging messages
+            msgs (list): list of messages for the log file.
+        """
+
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """ function to add a message to be written to the log file.
+
+        Args:
+            msg (string): message to queue for the log file.
+            timestampe (time): time value to assign to log mesasge, or None for
+                                current time.
+
+        Returns:
+            nothing.
+
+        Examples:
+            >>> cl = CustomLogger()
+            >>> cl.log('this is a message for the log')
+        """
         if timestamp is None:
             timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """ writes out all messages queued for the log file and closes the file.
+
+        Args:
+            none.
+
+        Returns:
+            nothing.
+
+        Examples:
+            >>> cl = CustomLogger()
+            >>> cl.flush()
+        """
         handled = []
 
-        fhandler = open(self.logfilename, 'a')
-        for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError as ioe:
+            self.log('I/O error {0} openning {1}, {2}'.format(ioe.errno,
+                                                              self.logfilename,
+                                                              ioe.strerror))
+            raise ioe
 
-        fhandler.close()
+        try:
+            for index, entry in enumerate(self.msgs):
+                fhandler.write(str(entry) + '\n')
+                handled.append(index)
+        except IOError as ioe:
+            self.log('I/O error {0} writting to log, {1}'.format(ioe.errno,
+                                                                 ioe.strerror))
 
-        for index in handled[::-1]:
-            del self.msgs[index]
+        finally:
+            fhandler.close()
+
+            for index in handled[::-1]:
+                del self.msgs[index]
