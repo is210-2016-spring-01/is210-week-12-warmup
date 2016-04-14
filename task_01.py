dd46ac09093df4fbc0bfe0e137077141d752abd2
paisely65 <cunysps@gmail.com>
2016-04-14 11:53:19 -0400

    task 1 revsed.

--- task_01.py
+++ task_01.py
@@ -4,14 +4,28 @@
 
 
 def simple_lookup(var1, var2):
-    var2 = [apple, grapes, oranges]
-    var1 = []
-    z = var1.update(var2) 
+    """This docstring illustrates the error handling principle.
+
+    Args:
+        var1(mixed): A dictionary
+        var2 (empty): A empty dictionary that will not return item accessed.
+
+    Returns:
+        mixed: Key/value accessed or a warning statement.
+
+    Examples:
+        >>>simple_lookup([a:3, 5, 7], [1, 2]
+        Warning: Your index/key does not exist.
+        [3, 5, 7]
+
+        >>>simple look_up([3, 5, 7], [a:1]
+        Warning: Your index/key does not exist.
+        [3, 5 7]
+        """
     try:
-        var1[dinner]
-    except KeyError:
+       var1[var2] 
+    except LookUpError:
         print 'Warning: Your index/key does not exist.'
-        var1 = [dinner]
 
     return var1
 
