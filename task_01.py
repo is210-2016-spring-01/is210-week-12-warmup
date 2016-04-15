0c610191ed486179064fffa4369e27b365bceb63
Daniella Sapozhnikova <daniellasapo@gmail.com>
2016-04-15 04:17:05 +0000

    just one

--- task_01.py
+++ task_01.py
@@ -4,4 +4,10 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """exception handling"""
+    try:
+        return var1[var2]
+    except:
+        print "Warning: Your index/key doesn't exist."
+        return var1
+
