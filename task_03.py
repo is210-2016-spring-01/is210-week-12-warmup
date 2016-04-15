6761bb3699fbe6d3f424477d7b0186cf17bd0892
paisely65 <cunysps@gmail.com>
2016-04-14 22:28:14 -0400

    Task_03.

--- task_03.py
+++ task_03.py
@@ -7,26 +7,47 @@ import time
 
 
 class CustomLogger(object):
-    """THis class creates logs."""
+    """This class creates logs."""
 
     def __init__(self, logfilename):
+        """Creates Logfilemane.
+        Attributes:
+            None
+        Arguments:
+            logfilename(string)
+            msgs(list)
+        """
         self.logfilename = logfilename
         self.msgs = []
 
     def log(self, msg, timestamp=None):
+        """Creates a timestamp.
+        Attributes:
+            None
+        Arguments:
+            msgs(list)
+            timestamp(integer)
+        """
         if timestamp is None:
-            timestamp = time.time()
+           timestamp = time.time()
         self.msgs.append((timestamp, msg))
 
     def flush(self):
+        """A flush function."""
         handled = []
-
-        fhandler = open(self.logfilename, 'a')
-        for index, entry in enumerate(self.msgs):
-            fhandler.write(str(entry) + '\n')
-            handled.append(index)
-
-        fhandler.close()
-
+        try:
+            fhandler = open(self.logfilename, 'a')
+        except IOError:
+            self.log('Logfile cannot be opened.')
+            raise IOError
+        else:
+            for index, entry in enumerate(self.msgs):
+                try:
+                    fhandler.write(str(entry) + '\n')
+                    handled.append(index)
+                except IOError:
+                    self.log('I/O error. File cannot be written to.')
+       
         for index in handled[::-1]:
             del self.msgs[index]
+        fhandler.close()
