5c73ae08447d8c56c00cdcffacc7693876571479
Sylvia Guzikowski <xiran-aneki@live.com>
2016-04-16 07:40:16 +0000

    homework

--- task_02.py
+++ task_02.py
@@ -7,8 +7,19 @@ import datetime
 
 
 class InvalidAgeError(Exception):
+    """class docstring for an exception"""
     pass
 
 def get_age(birthyear):
+    """Tests if age provided is greater than 0.
+    Attributes:
+        birthyear (int): year that person was born
+    Returns:
+        age (int): person's age or error if less than 0
+    Example:
+        >>>get_age(2000)
+        16
     age = datetime.datetime.now().year - birthyear
+    if age < 0:
+        raise InvalidAgeError()
     return age
