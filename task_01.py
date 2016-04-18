8c501f76e14e4b54af12f27b5a5b06f2fc87cc42
jaiz25 <jaizelle@gmail.com>
2016-04-18 03:26:25 +0000

    task1

--- task_01.py
+++ task_01.py
@@ -4,4 +4,26 @@
 
 
 def simple_lookup(var1, var2):
-    return var1[var2]
+    """A function that attempts to access any index or key.
+
+    Args:
+        var1 (mixed): any value
+        var2 (mixed): any value
+
+    Return:
+        var1: any value
+
+    Examples:
+
+    >>> simple_lookup([1,2], 4)
+    Warning: Your index/key doesn't exist.
+    [1,2]
+    >>> simple_lookup({}, 'banana')
+    Warning: Your index/key doesn't exist.
+    {}
+    """
+    try:
+        return var1[var2]
+    except LookupError:
+        print "Warning: Your index/key doesn't exist."
+        return var1
